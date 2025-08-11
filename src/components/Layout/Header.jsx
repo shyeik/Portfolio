998291caa62e007dd2ca5955ef4229d8fff6371c
Jake Declock <derilojake2528@gmail.com>
2025-08-12 02:31:44 +0800

    ddadadda

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed w-full z-10 mb-10 md:mb-0 transition-colors duration-300 ${
+      className={`fixed w-full z-10 transition-colors duration-300 ${
         isScrolled ? "bg-[#1C1C1C] shadow-md" : "bg-transparent"
       }`}
     >
@@ -57,7 +57,7 @@ const Header = () => {
                  0l5-5m-5 5V4"
             />
           </svg>
-          Download My Resume
+          My Resume
         </a>
       </nav>
     </header>
